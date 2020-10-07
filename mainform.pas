93db3d4d1b1fb96b3f63ec0f66e3d40bc0dfc6d8
Ivan Semenkov <ivan@semenkov.pro>
2020-10-07 14:35:30 +0300

    Update submodules

---
+++ mainform.pas
@@ -0,0 +1,31 @@
+unit MainForm;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+end.
+
