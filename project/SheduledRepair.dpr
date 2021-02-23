a3b7056d5fcb26fc5c380f4cda18cd0fa956a843
Ivan Semenkov <ivan@semenkov.pro>
2021-02-23 17:34:17 +0200

    Update submodules

--- project/SheduledRepair.dpr
+++ project/SheduledRepair.dpr
@@ -29,12 +29,7 @@ program SheduledRepair;
 
 uses
   Vcl.Forms,
-  mainform_delphi in '..\forms\mainform_delphi.pas' {MainWindow},
-  jobform_delphi in '..\forms\editors\jobform_delphi.pas' {JobWindow},
-  equipmentform_delphi in '..\forms\editors\equipmentform_delphi.pas' {EquipmentWindow},
-  entityform_delphi in '..\forms\editors\entityform_delphi.pas' {EntityWindow},
-  nodeform_delphi in '..\forms\editors\nodeform_delphi.pas' {NodeWindow},
-  greasebundleform_delphi in '..\forms\editors\greasebundleform_delphi.pas' {GreaseBundleWindow};
+  MainWindowUnit in '..\forms\MainWindowUnit.pas' {MainWindow};
 
 {$R *.res}
 
@@ -42,10 +37,5 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TMainWindow, MainWindow);
-  Application.CreateForm(TJobWindow, JobWindow);
-  Application.CreateForm(TEquipmentWindow, EquipmentWindow);
-  Application.CreateForm(TEntityWindow, EntityWindow);
-  Application.CreateForm(TNodeWindow, NodeWindow);
-  Application.CreateForm(TGreaseBundleWindow, GreaseBundleWindow);
   Application.Run;
 end.
